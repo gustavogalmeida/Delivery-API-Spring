174ab9c6069e4ed4c9ff4a1f089b11fa4f8d7c4b
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2023-02-07 20:34:02 -0300

    Spring Initializr

---
+++ src/main/java/com/gustavogalmeida/delivapi/DelivapiApplication.java
@@ -0,0 +1,13 @@
+package com.gustavogalmeida.delivapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelivapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavogalmeida/delivapi/DelivapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavogalmeida.delivapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelivapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
