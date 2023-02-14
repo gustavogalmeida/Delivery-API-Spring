d4a956c727ffb6f97f1ef1bc774ad12b6b4a36ba
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2023-02-13 21:19:27 -0300

    Project creation

---
+++ src/test/java/com/gustavoalmeida/deliveryapi/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoalmeida.deliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
