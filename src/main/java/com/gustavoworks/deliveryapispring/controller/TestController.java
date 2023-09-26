92c84eb7cdc0f92cf6e108e6b6df6c2c65965f76
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2023-09-25 21:22:19 -0300

    Criado primeiro controller para testar aplicação

---
+++ src/main/java/com/gustavoworks/deliveryapispring/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.gustavoworks.deliveryapispring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello!";
+    }
+}
